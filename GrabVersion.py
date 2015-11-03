0bc41ea6c79add05d93555415d6ab7c9b6f81b17
CoeusITE <CoeusITE@gmail.com>
2015-11-03 16:47:37 -0500

    verison file for win32

---
+++ GrabVersion.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1,1,0,0),
+    prodvers=(1,1,0,0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'CoeusITE@Github'),
+        StringStruct(u'FileDescription', u'Image Recognition Tool for Poketoru'),
+        StringStruct(u'FileVersion', u'1.1..00'),
+        StringStruct(u'InternalName', u'cmd'),
+        StringStruct(u'LegalCopyright', u'Copyright 2015 CoeusITE.'),
+        StringStruct(u'OriginalFilename', u'Cmd.Exe'),
+        StringStruct(u'ProductName', u'ShuffleIR-3DS'),
+        StringStruct(u'ProductVersion', u'1.1.0.0')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
