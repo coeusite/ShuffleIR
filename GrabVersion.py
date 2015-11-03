4df84eb74edb5327339852e9d05cf5292d20baef
CoeusITE <CoeusITE@gmail.com>
2015-11-03 16:55:42 -0500

    verison 1.1.0.0

--- GrabVersion.py
+++ GrabVersion.py
@@ -16,12 +16,12 @@ VSVersionInfo(
         u'040904B0',
         [StringStruct(u'CompanyName', u'CoeusITE@Github'),
         StringStruct(u'FileDescription', u'Image Recognition Tool for Poketoru'),
-        StringStruct(u'FileVersion', u'1.1..00'),
+        StringStruct(u'FileVersion', u'1, 1, 0, 0'),
         StringStruct(u'InternalName', u'cmd'),
         StringStruct(u'LegalCopyright', u'Copyright 2015 CoeusITE.'),
         StringStruct(u'OriginalFilename', u'Cmd.Exe'),
         StringStruct(u'ProductName', u'ShuffleIR-3DS'),
-        StringStruct(u'ProductVersion', u'1.1.0.0')])
+        StringStruct(u'ProductVersion', u'1, 1, 0, 0')])
       ]), 
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
